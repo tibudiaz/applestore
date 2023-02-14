bcde0141982ad91c0645fa4db8f0b31ef730d00f
tibudiaz <augusto_diaz@outlook.com>
2023-02-14 00:40:55 -0300

    firebase

--- assets/js/catalogo.js
+++ assets/js/catalogo.js
@@ -1,100 +1,94 @@
-  // Initialize Firebase
-  var firebaseConfig = {
-    apiKey: "AIzaSyBa6Op4MQHY5kaeCOBHfRuw_VHSXgbcYn0",
-    authDomain: "apple-55adb.firebaseapp.com",
-    databaseURL: "https://apple-55adb-default-rtdb.firebaseio.com",
-    projectId: "apple-55adb",
-    storageBucket: "apple-55adb.appspot.com",
-    messagingSenderId: "797079389741",
-    appId: "1:797079389741:web:729e6868a42410f1341aa4",
-    measurementId: "G-9G30DFC6MX"
-  };
-  if (firebase.apps.length === 0) {
-    firebase.initializeApp(firebaseConfig);
-}
-  const formProduct = document.querySelector(".form-product");
-  if (formProduct) {
-    formProduct.addEventListener("submit", function (event) {
-        event.preventDefault();
-  
-        const productName = formProduct.querySelector("#product-name").value;
-        const productImage = formProduct.querySelector("#product-image").value;
-        const productPrice = formProduct.querySelector("#product-price").value;
-        const product = {
-          name: productName,
-          image: productImage,
-          price: productPrice,
-        };
-  
-        // Initialize Firebase if it hasn't been done yet
+const firebaseConfig = {
 
+    apiKey: "AIzaSyBa6Op4MQHY5kaeCOBHfRuw_VHSXgbcYn0",
   
-        // Save the product in the database
-        database.ref().push();
-    });
+    authDomain: "apple-55adb.firebaseapp.com",
+  
+    databaseURL: "https://apple-55adb-default-rtdb.firebaseio.com",
+  
+    projectId: "apple-55adb",
+  
+    storageBucket: "apple-55adb.appspot.com",
+  
+    messagingSenderId: "797079389741",
+  
+    appId: "1:797079389741:web:729e6868a42410f1341aa4",
+  
+    measurementId: "G-9G30DFC6MX"
+  
+  };
+// Inicializa la aplicación de Firebase con la configuración proporcionada
+firebase.initializeApp(firebaseConfig);  
+
+
+
+
+//funcion para leer lo cargado en el local storage
+function loadProducts() {
+    // Intentamos obtener los productos del local storage
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  
+    // Si no encontramos los productos en el local storage, los obtenemos de Firebase
+    if (cart.length === 0) {
+      firebase.database().ref("cart").once("value")
+        .then(snapshot => {
+          snapshot.forEach(productSnapshot => {
+            const product = productSnapshot.val();
+            cart.push(product);
+          });
+          // Llamamos a la función addProduct para agregar cada producto al catálogo
+          cart.forEach(product => addProduct(product.name, product.price, product.image));
+        });
+    } else {
+      // Si encontramos los productos en el local storage, los agregamos al catálogo directamente
+      cart.forEach(product => addProduct(product.name, product.price, product.image));
+    }
   }
 
+//llamamos a la api del precio del dolar 
+function getExchangeRate() {
+    return fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
+      .then(response => response.json())
+      .then(data => parseFloat(data.filter(x => x.casa.nombre === 'Dolar Blue')[0].casa.venta.replace(',', '')));
+  }
+  //tomamos el precio añadido por el usuario (en dolares) y lo pasamos a pesos segun cotizacion de mi ciudad del dolar. (por eso a exchangeRate le sumo $4)
+  function addProduct(name, price) {
+    getExchangeRate().then(exchangeRate => {
+      const arsPrice = (price / 100) * (exchangeRate + 400);
 
-// Obtener una referencia al servicio de base de datos
-var database = firebase.database();
-const productsRef = database.ref();
+        // Continuamos con la lógica de la función
+        const productContainer = document.createElement('div');
+        productContainer.classList.add('product-card');
+        const productImage = document.createElement('img');
+        switch (name.toLowerCase()) {
+            case 'iphone 13 pro max':
+                productImage.src = 'assets/img/header_iphone_13_pro_sierra_blue_large_2x-1.jpg';
+            break;
+            case 'iphone 12 pro max':
+                productImage.src = 'assets/img/iph12pm.png';
+            break;
+            default:
+                productImage.src = 'assets/img/png-clipart-candied-apples-apple-logo.png';
+        }
 
-function loadProducts() {
-  productsRef.on("value", function (snapshot) {
-    const products = snapshot.val();
-    for (let key in products) {
-      const product = products[key];
-      addProduct(product.name, product.price);
-    }
+        const productName = document.createElement('h3');
+        productName.innerText = name;
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = `Precio: ARS $${arsPrice.toLocaleString()}`;
+        const btnComprar = document.createElement("button");
+        btnComprar.classList.add("btn-comprar");
+        btnComprar.innerText = "Ver mas";
+        productContainer.appendChild(productImage);
+        productContainer.appendChild(productName);
+        productContainer.appendChild(productPrice);
+        productContainer.appendChild(btnComprar);
+        const catalogSection = document.querySelector('#catalog');
+        catalogSection.appendChild(productContainer);
+    });
+}
+
+// Llamamos a la función loadProducts al cargar la página
+window.addEventListener("load", function () {
+    loadProducts();
   });
-}
-
-function addProduct(name, price) {
-    const fetchUrl = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales';
-    fetch(fetchUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`Error al obtener la tasa de cambio. Estado HTTP: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            const exchangeRate = data.filter(x => x.casa.nombre === 'Dolar Blue')[0].casa.venta;
-            const arsPrice = (price / 100) * parseFloat(exchangeRate.replace(',', ''));
-
-            const productContainer = document.createElement('div');
-            productContainer.classList.add('product-card');
-
-            const productImage = document.createElement('img');
-            switch (name.toLowerCase()) {
-                case 'iphone 13 pro max':
-                    productImage.src = 'assets/img/header_iphone_13_pro_sierra_blue_large_2x-1.jpg';
-                    break;
-                case 'iphone 12 pro max':
-                    productImage.src = 'assets/img/iph12pm.png';
-                    break;
-                default:
-                    productImage.src = 'assets/img/png-clipart-candied-apples-apple-logo.png';
-            }
-            productContainer.appendChild(productImage);
-
-            const productName = document.createElement('h3');
-            productName.innerText = name;
-            productContainer.appendChild(productName);
-
-            const productPrice = document.createElement('p');
-            productPrice.innerText = `Precio: ARS $${arsPrice.toLocaleString()}`;
-            productContainer.appendChild(productPrice);
-
-            const btnComprar = document.createElement("button");
-            btnComprar.classList.add("btn-comprar");
-            btnComprar.innerText = "Ver mas";
-            productContainer.appendChild(btnComprar);
-
-            const catalogSection = document.querySelector('#catalog');
-            catalogSection.appendChild(productContainer);
-        })
-        .catch(error => {
-            console.error(error);
-        });
-}
--- assets/js/catalogo1.js
+++ assets/js/catalogo1.js
@@ -1,35 +1,86 @@
-  // Initialize Firebase
-  var firebaseConfig = {
-    apiKey: "AIzaSyBa6Op4MQHY5kaeCOBHfRuw_VHSXgbcYn0",
-    authDomain: "apple-55adb.firebaseapp.com",
-    databaseURL: "https://apple-55adb-default-rtdb.firebaseio.com",
-    projectId: "apple-55adb",
-    storageBucket: "apple-55adb.appspot.com",
-    messagingSenderId: "797079389741",
-    appId: "1:797079389741:web:729e6868a42410f1341aa4",
-    measurementId: "G-9G30DFC6MX"
-  };
-  if (firebase.apps.length === 0) {
-    firebase.initializeApp(firebaseConfig);
-}
-  const formProduct = document.querySelector(".form-product");
-  if (formProduct) {
-    formProduct.addEventListener("submit", function (event) {
-        event.preventDefault();
-  
-        const productName = formProduct.querySelector("#product-name").value;
-        const productImage = formProduct.querySelector("#product-image").value;
-        const productPrice = formProduct.querySelector("#product-price").value;
-        const product = {
-          name: productName,
-          image: productImage,
-          price: productPrice,
-        };
-  
-        // Initialize Firebase if it hasn't been done yet
+const firebaseConfig = {
 
-  
-        // Save the product in the database
-        database.ref().push();
+  apiKey: "AIzaSyBa6Op4MQHY5kaeCOBHfRuw_VHSXgbcYn0",
+
+  authDomain: "apple-55adb.firebaseapp.com",
+
+  databaseURL: "https://apple-55adb-default-rtdb.firebaseio.com",
+
+  projectId: "apple-55adb",
+
+  storageBucket: "apple-55adb.appspot.com",
+
+  messagingSenderId: "797079389741",
+
+  appId: "1:797079389741:web:729e6868a42410f1341aa4",
+
+  measurementId: "G-9G30DFC6MX"
+
+};
+
+
+
+
+//tomamos los datos cargados en el formulario por el usuario y los almacenamos en el local storage
+const formProduct = document.querySelector(".form-product");
+formProduct.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const productName = formProduct.querySelector("#product-name").value;
+  const productImage = formProduct.querySelector("#product-image").value;
+  const productPrice = formProduct.querySelector("#product-price").value;
+
+  const product = {
+    name: productName,
+    image: productImage,
+    price: productPrice,
+  };
+
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+  cart.push(product);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  sendDataToFirebase(product);
+  formProduct.reset();
+});
+
+// Inicializa la aplicación de Firebase con la configuración proporcionada
+firebase.initializeApp(firebaseConfig);
+
+// Define la función que enviará los datos a Firebase
+function sendDataToFirebase(product) {
+  // Obtiene una referencia a la base de datos de Firebase
+  const database = firebase.database();
+
+  // Envía el nuevo producto a Firebase
+  database.ref('cart').push(product, error => {
+    if (error) {
+      console.error('Error al enviar los datos a Firebase:', error);
+    } else {
+      console.log('Los datos se enviaron correctamente a Firebase.');
+    }
+  });
+}
+
+// Carga los productos desde Firebase al cargar la página
+function loadProducts() {
+  // Intentamos obtener los productos del Local Storage
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  // Obtenemos los productos de Firebase y los agregamos al Local Storage si no se encontraron allí antes
+  firebase.database().ref("cart").once("value")
+    .then(snapshot => {
+      snapshot.forEach(productSnapshot => {
+        const product = productSnapshot.val();
+        if (!cart.find(item => item.name === product.name)) {
+          cart.push(product);
+        }
+      });
+      localStorage.setItem("cart", JSON.stringify(cart));
+      cart.forEach(product => addProduct(product.name, product.price, product.image));
     });
-  }
+}
+
+// Llamamos a la función loadProducts al cargar la página
+window.addEventListener("load", function () {
+  loadProducts();
+});
